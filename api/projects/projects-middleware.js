4f50761165068fb881c6d8dae4c27d92817c969e
Cori Wooley <wooleycori@gmail.com>
2022-06-08 05:15:03 -0800

    projectsROuter get, getbyid, post, finished projects middleware

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -6,13 +6,32 @@ function logger(req, res, next) {
     next()
   }
 
-  function validateId(req, res, next){
-      //do something here
-      next()
+  async function validateId(req, res, next){
+      try {
+          const project = await Projects.get(req.params.id)
+          if(!project){
+              res.status(404).json({
+                  message: 'not found'
+              })
+          } else {
+              req.project = project
+              next()
+          }
+      } catch(err){
+          next(err)
+      }
   }
 
   function validatePost(req, res, next){
-      next()
+      const {name, description} = req.body
+      if(!name || !description){
+          res.status(400).json({
+              message: "missing required fields"
+          })
+      } else {
+          name, description
+          next()
+      }
   }
 
   module.exports = {
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -5,16 +5,28 @@ const md = require('./projects-middleware')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    console.log(req.method)
+router.get('/', (req, res, next) => {
+    Projects.get()
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(next)
 })
 
-router.get('/:id', (req, res) => {
-    console.log(req.method)
+router.get('/:id', md.validateId, (req, res) => {
+    res.json(req.project)
 })
 
-router.post('/', (req, res) => {
-    console.log(req.method)
+router.post('/', md.validatePost, (req, res, next) => {
+    Projects.insert({
+        name: req.body.name,
+        description: req.body.description,
+        completed: req.body.completed
+    })
+    .then(project => {
+        res.status(201).json(project)
+    })
+    .catch(next)
 })
 
 router.put('/:id', (req, res) => {
@@ -29,6 +41,13 @@ router.get('/:id/actions', (req, res) => {
     console.log(req.method)
 })
 
-
+router.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+        message1: "There was an error performing the required operation",
+        message2: err.message
+    })
+    next()
+})
 
 module.exports = router
