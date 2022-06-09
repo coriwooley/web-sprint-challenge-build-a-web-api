27131644b6c034977ad0fe717feb4545c57ea8aa
Cori Wooley <wooleycori@gmail.com>
2022-06-09 01:30:41 -0800

    made GET & GET:id for actions router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -5,7 +5,28 @@ const md = require('./actions-middlware')
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    Actions.get()
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+})
 
+router.get('/:id', md.validateId, (req, res, next) => {
+    res.json(req.action)
+})
+
+router.post('/', (req, res) => {
+    //do something here
+})
+
+router.put('/:id', (req, res) => {
+    //do something here
+})
+
+router.delete('/:id', (req, res) => {
+    //do something here
+})
 
 
 
